fdc5a1cdb8ee5eb19b3784ed92827dc879252e7f
satkumar09 <kumarsatya626@gmail.com>
2023-08-04 16:16:06 +0530

    local

--- server/schema/student.js
+++ server/schema/student.js
@@ -1,20 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.Schema({
+const studentSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        uppercase: true,
-        required: true
+      type: String,
+      uppercase: true,
+      required: true,
     },
     age: {
-        type: Number,
-        min: 12,
-        required: true
+      type: Number,
+      min: 12,
+      required: true,
     },
     dob: {
-        type: String,
-        required: true
-    }
-}, { collection: 'student', timestamps: true });
+      type: String,
+      required: true,
+    },
+    roll: {
+      type: Number,
+      required: true,
+    },
+  },
+  { collection: "student", timestamps: true }
+);
 
-module.exports = mongoose.model('Student', studentSchema);
+module.exports = mongoose.model("Student", studentSchema);
